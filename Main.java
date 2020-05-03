d9cc0cf132c5603f9bc3713355bcdd023fd7a535
supahstarclod <621383@gmail.com>
2020-05-02 19:32:45 -0700

    implemented functional GUI (v3)

--- Main.java
+++ Main.java
@@ -7,15 +7,18 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 public class Main extends JFrame {
-	String mode;
-	double r2x, g2x, b2x, r2y, g2y, b2y;
+	static String mode;
+	static double r2x, g2x, b2x, alpha;
+	static JTextField r1b, g1b, b1b, r1t, g1t, b1t, r2b, g2b, b2b, r2t, g2t, b2t;
+	static JLabel label1, label2;
+	static JPanel sublabel1, sublabel2;
 	
 	public Main() {
 		
 		ArrayList<Image> list = new ArrayList<Image>();
 		String[] modeList = {"Multiply", "Normal", "Overlay", "Screen", "Shade"};
 		mode = "Normal";
-		double r2x, g2x, b2x, r2y, g2y, b2y = 0;
+		alpha = 1;
 		ImageIcon icon = new ImageIcon("peri16.png");
 		ImageIcon icon2 = new ImageIcon("peri48.png");
 		Image p1 = icon.getImage();
@@ -34,6 +37,9 @@ public class Main extends JFrame {
 		content2.setLayout(new FlowLayout());
 		JPanel content3 = new JPanel();
 		content3.setLayout(new FlowLayout());
+		JPanel content4 = new JPanel();
+		content4.setLayout(new FlowLayout());
+		
 		//content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
 		//content.setLayout(new GridLayout(5,1));
 		//content.setLayout(new FlowLayout());
@@ -42,9 +48,9 @@ public class Main extends JFrame {
 		subpanel1.setBorder(BorderFactory.createTitledBorder("[1] Base color"));
 		BoxLayout sp1 = new BoxLayout(subpanel1, BoxLayout.X_AXIS);
 		subpanel1.setLayout(sp1);
-		JTextField r1b = new JTextField(3);
-		JTextField g1b = new JTextField(3);
-		JTextField b1b = new JTextField(3);
+		r1b = new JTextField(3);
+		g1b = new JTextField(3);
+		b1b = new JTextField(3);
 		JButton set1 = new JButton("Set");
 		subpanel1.add(r1b);
 		subpanel1.add(g1b);
@@ -55,9 +61,9 @@ public class Main extends JFrame {
 		subpanel2.setBorder(BorderFactory.createTitledBorder("Composite color"));
 		BoxLayout sp2 = new BoxLayout(subpanel2, BoxLayout.X_AXIS);
 		subpanel2.setLayout(sp2);
-		JTextField r1t = new JTextField(3);
-		JTextField g1t = new JTextField(3);
-		JTextField b1t = new JTextField(3);
+		r1t = new JTextField(3);
+		g1t = new JTextField(3);
+		b1t = new JTextField(3);
 		JButton set2 = new JButton("Set");
 		subpanel2.add(r1t);
 		subpanel2.add(g1t);
@@ -68,17 +74,17 @@ public class Main extends JFrame {
 		modes.setSelectedIndex(1);
 
 		JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 100, 100);
-		slider.setMajorTickSpacing(50);
-		slider.setMinorTickSpacing(10);
+		slider.setMajorTickSpacing(25);
+		slider.setMinorTickSpacing(5);
 		slider.setSnapToTicks(true);
 		slider.setPaintTicks(true);
 		//---//
 		Hashtable<Integer, JLabel> labelTable = new Hashtable<Integer, JLabel>();
-		labelTable.put(0, new JLabel("0"));
+		//labelTable.put(0, new JLabel("0"));
 		//labelTable.put(50, new JLabel("0.5"));
-		labelTable.put(100, new JLabel("1"));
-		slider.setLabelTable(labelTable);
-		slider.setPaintLabels(true);
+		//labelTable.put(100, new JLabel("1"));
+		//slider.setLabelTable(labelTable);
+		//slider.setPaintLabels(true);
 		//---//
 		JLabel value = new JLabel();
 		value.setHorizontalAlignment(JLabel.CENTER);
@@ -88,9 +94,9 @@ public class Main extends JFrame {
 		subpanel3.setBorder(BorderFactory.createTitledBorder("[2] Base color"));
 		BoxLayout sp3 = new BoxLayout(subpanel3, BoxLayout.X_AXIS);
 		subpanel3.setLayout(sp3);
-		JTextField r2b = new JTextField(3);
-		JTextField g2b = new JTextField(3);
-		JTextField b2b = new JTextField(3);
+		r2b = new JTextField(3);
+		g2b = new JTextField(3);
+		b2b = new JTextField(3);
 		JButton set3 = new JButton("Set");
 		subpanel3.add(r2b);
 		subpanel3.add(g2b);
@@ -101,32 +107,44 @@ public class Main extends JFrame {
 		subpanel4.setBorder(BorderFactory.createTitledBorder("Composite color"));
 		BoxLayout sp4 = new BoxLayout(subpanel4, BoxLayout.X_AXIS);
 		subpanel4.setLayout(sp4);
-		JTextField r2t = new JTextField(3);
-		JTextField g2t = new JTextField(3);
-		JTextField b2t = new JTextField(3);
+		r2t = new JTextField(3);
+		g2t = new JTextField(3);
+		b2t = new JTextField(3);
 		JButton set4 = new JButton("Set");
 		subpanel4.add(r2t);
 		subpanel4.add(g2t);
 		subpanel4.add(b2t);
 		subpanel4.add(set4);
 		
-		set1.addActionListener(new ActionListener() {
+		sublabel1 = new JPanel();
+		sublabel1.setBorder(BorderFactory.createTitledBorder(""));
+		label1 = new JLabel();
+		label1.setText(" --, --, -- ");
+		sublabel1.add(label1);
+		
+		sublabel2 = new JPanel();
+		sublabel2.setBorder(BorderFactory.createTitledBorder(""));
+		label2 = new JLabel();
+		label2.setText(" --, --, -- ");
+		sublabel2.add(label2);
+		
+		ActionListener setter = new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
-				try {
-					double r1a = Double.parseDouble(r1b.getText());
-					double g1a = Double.parseDouble(g1b.getText());
-					double b1a = Double.parseDouble(b1b.getText());
-				}
-				catch (Exception e) {
-					System.out.println(e);
-				}
+				change();
 			}
-		});
+		};
+		
+		set1.addActionListener(setter);
+		set2.addActionListener(setter);
+		set3.addActionListener(setter);
+		set4.addActionListener(setter);
+		//slider.addChangeListener((ChangeListener) setter);
 		
 		modes.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
 				JComboBox cb = (JComboBox) event.getSource();
 				mode = (String) cb.getSelectedItem();
+				change();
 			}
 		});
 		
@@ -139,11 +157,11 @@ public class Main extends JFrame {
 				if (source.getValue() < 100)
 					output += "‏‏‎ ";
 				value.setText(output + Integer.toString((int) source.getValue()) + "%");
+				alpha = (double) source.getValue() / 100;
+				change();
 			}
 		});
 		
-		
-		//---------------------\\
 		content.add(subpanel1);
 		content.add(subpanel2);
 		content2.add(modes);
@@ -151,58 +169,111 @@ public class Main extends JFrame {
 		content2.add(value);
 		content3.add(subpanel3);
 		content3.add(subpanel4);
+		content4.add(sublabel1);
+		content4.add(sublabel2);
 		mainContent.add(content);
 		mainContent.add(content2);
 		mainContent.add(content3);
+		mainContent.add(content4);
 		add(mainContent);
 		
-		setTitle("Color Analyzer");
-		setSize(500, 300);
+		setTitle("Color Finder");
+		setSize(475, 270);
 		setLocation(0, 0);
 		setVisible(true);
-		
-		//v2
-		/*content.setLayout(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
-		
-		JTextField t1 = new JTextField(10);
-		t1.setVisible(true);
-		gbc.fill = GridBagConstraints.HORIZONTAL;
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		content.add(t1, gbc);
-		add(content);*/
-		
-		//v1
-		//Container contentPane = getContentPane();
-		/*JButton myButton = new JButton("Button");
-		content.add(myButton);
-		setContentPane(content);*/
-		
-		/*Panel displayPanel = new Panel(new FlowLayout());
-		TextField r = new TextField("", 4);
-		TextField b = new TextField("", 4);
-		TextField g = new TextField("", 4);
-		displayPanel.add(r);
-		displayPanel.add(g);
-		displayPanel.add(b);
-		add(displayPanel);
-		setVisible(true);*/
 	}
 	
-	public void select1(double r, double g, double b, double a) {
-		if (mode.equals("Multiply")) {
+	public static void change() {
+		double r1, g1, b1, r3, g3, b3 = -1;
+		try {
+			r1 = Double.parseDouble(r1b.getText());
+			g1 = Double.parseDouble(g1b.getText());
+			b1 = Double.parseDouble(b1b.getText());
+			r3 = Double.parseDouble(r1t.getText());
+			g3 = Double.parseDouble(g1t.getText());
+			b3 = Double.parseDouble(b1t.getText());
+			select(r1, g1, b1, r3, g3, b3);
+			label1.setText(String.format("%d, %d, %d", (int) r2x, (int) g2x, (int) b2x));
+			try {
+				sublabel1.setBorder(BorderFactory.createLineBorder(new Color((int) r2x, (int) g2x, (int) b2x), 1));
+			}
+			catch (Exception e) {
+				sublabel1.setBorder(BorderFactory.createTitledBorder(""));
+			}
+			//label1.setOpaque(true);
+			//label1.setBackground();
+			
+			r1 = Double.parseDouble(r2b.getText());
+			g1 = Double.parseDouble(g2b.getText());
+			b1 = Double.parseDouble(b2b.getText());
+			r3 = Double.parseDouble(r2t.getText());
+			g3 = Double.parseDouble(g2t.getText());
+			b3 = Double.parseDouble(b2t.getText());
+			select(r1, g1, b1, r3, g3, b3);
+			label2.setText(String.format("%d, %d, %d", (int) r2x, (int) g2x, (int) b2x));
+			try {
+				sublabel2.setBorder(BorderFactory.createLineBorder(new Color((int) r2x, (int) g2x, (int) b2x), 1));
+			}
+			catch (Exception e) {
+				sublabel2.setBorder(BorderFactory.createTitledBorder(""));
+			}
+			//label2.setOpaque(true);
+			//label2.setBackground(new Color((int) r2x, (int) g2x, (int) b2x));
 		}
+		catch (Exception e) {
+			System.out.println(e);
+			//label1.setOpaque(false);
+			//label2.setOpaque(false);
+		}
+	};
+	
+	public static void select(double r1, double g1, double b1, double r3, double g3, double b3) {
+		System.out.println(mode);
+		if (mode.equals("Multiply")) {
+			r2x = multiply(r1, r3, alpha);
+			g2x = multiply(g1, g3, alpha);
+			b2x = multiply(b1, b3, alpha);
+		}
+		else if (mode.equals("Normal")) {
+			r2x = normal(r1, r3, alpha);
+			g2x = normal(g1, g3, alpha);
+			b2x = normal(b1, b3, alpha);
+		}
+		else if (mode.equals("Overlay")) {
+			r2x = overlay(r1, r3, alpha);
+			g2x = overlay(g1, g3, alpha);
+			b2x = overlay(b1, b3, alpha);
+		}
+		else if (mode.equals("Shade")) {
+			r2x = shade(r1, r3, alpha);
+			g2x = shade(g1, g3, alpha);
+			b2x = shade(b1, b3, alpha);
+		}
+		System.out.println(r1 + " " + r3);
 			
 	}
 	
-	public static double overlay(double v1, double v3, double alpha) {
+	public static double normal(double v1, double v3, double a) {
+		return v1 + (v3-v1)/a;
+	}
+	
+	public static double multiply(double v1, double v3, double a) {
+		return 255 * (1 - (1 - v3 / v1) / a);
+	}
+	
+	public static double overlay(double v1, double v3, double a) {
 		double val = 0;
 		if (v1 < 128)
-			val = 255 * (1 - (1 - v3 / (2 * v1)) / alpha);
+			val = 255 * (1 - (1 - v3 / (2 * v1)) / a);
 		else
-			val = 255 * (((v3 - 255) + (1 - alpha) * (v3 - v1) / alpha) / (2 * (255 - v1)) + 1);	  
+			val = 255 * (((v3 - 255) + (1 - a) * (v3 - v1) / a) / (2 * (255 - v1)) + 1);	  
 		return val;
+	}
+	
+	public static double shade(double v1, double v3, double a) {
+		if (v3 == 0)
+			System.out.println("Error: Values below 0");
+		return (255 * a - v1 + v3) / a;
 	}
 		
 	public static void main(String[] args) {
@@ -230,14 +301,14 @@ public class Main extends JFrame {
 		double alpha = arr.get(6);
 		
 		if (type.equals("n")) {
-			r2 = r1 + (r3-r1)/alpha;
-			g2 = g1 + (g3-g1)/alpha;
-			b2 = b1 + (b3-b1)/alpha;
+			r2 = normal(r1, r3, alpha);
+			g2 = normal(g1, g3, alpha);
+			b2 = normal(b1, b3, alpha);
 		}
 		else if (type.equals("m")) {
-			r2 = 255 * (1 - (1 - r3 / r1) / alpha);
-			g2 = 255 * (1 - (1 - g3 / g1) / alpha);
-			b2 = 255 * (1 - (1 - b3 / b1) / alpha);
+			r2 = multiply(r1, r3, alpha);
+			g2 = multiply(g1, g3, alpha);
+			b2 = multiply(b1, b3, alpha);
 		}
 		else if (type.equals("o")) {
 			r2 = overlay(r1, r3, alpha);
@@ -245,11 +316,9 @@ public class Main extends JFrame {
 			b2 = overlay(b1, b3, alpha);
 		}
 		else if (type.equals("s")) {
-			if (r3 == 0 || g3 == 0 || b3 == 0)
-				System.out.println("Error: Values below 0");
-			r2 = (255 * alpha - r1 + r3) / alpha;
-			g2 = (255 * alpha - g1 + g3) / alpha;
-			b2 = (255 * alpha - b1 + b3) / alpha;
+			r2 = shade(r1, r3, alpha);
+			g2 = shade(g1, g3, alpha);
+			b2 = shade(b1, b3, alpha);
 		}
 		System.out.println(String.format("RGB: %f, %f, %f (%f)", r2, g2, b2, alpha));
 	}
